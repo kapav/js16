8fc72ad7135304e4e79288a8b5f7ea304fde683f
Александр Кочкин <Alexander.Kochkin@artezio.com>
2019-05-15 16:48:01 +0400

    Добавление и минификация изображения с помощью file-loader'а и image-webpack-loader'а.

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,28 @@ module.exports = {
 					'css-loader',
 					'sass-loader'
 				]
+			},
+			{
+				test: /\.(jpg|png|svg|gif)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: './',
+							useRelativePath: true
+						}
+					},
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							mozjpeg: {
+								progressive: true,
+								quality: 70
+							}
+						}
+					}
+				]
 			}
 		]
 	},
